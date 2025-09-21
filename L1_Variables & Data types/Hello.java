e14735885f7a1abb5cfbe92ec2033d16c4f42c44
Nikhil <nikhilgithub2005@gmail.com>
2025-09-21 19:16:53 +0530

    Learn about Variables and data types in Java

---
+++ L1_Variables & Data types/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main (String args[]){
+        System.out.println("Hello Wolrd With Java  ");
+        System.out.println("Hello Wolrd With C ");
+        System.out.println("Hello Wolrd With Python ");
+    }
+}

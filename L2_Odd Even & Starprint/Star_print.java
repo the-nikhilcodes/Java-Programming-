a0ec005fd55823e6fd1cff300f92f4b433faadc9
Nikhil <nikhilgithub2005@gmail.com>
2025-09-22 22:11:43 +0530

    Added Practice programs

---
+++ L2_Odd Even & Starprint/Star_print.java
@@ -0,0 +1,9 @@
+public class Star_print {
+   public static void main(String[] args) {
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("****");
+   } 
+
+}
